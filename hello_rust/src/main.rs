a2ab9cd0574fcc17f1650a401ee50d11985834d0
Alex Walcutt <awalcutt@csh.rit.edu>
2016-12-09 23:41:17 -0800

    Implements HTTP/2 demo via rust-solicit crate

--- hello_rust/src/main.rs
+++ hello_rust/src/main.rs
@@ -1,3 +1,39 @@
+extern crate solicit;
+use solicit::http::client::CleartextConnector;
+use solicit::client::SimpleClient;
+use std::str;
+
 fn main() {
-    println!("Hello Rust!");
+    // Connect to an HTTP/2 aware server
+    let connector = CleartextConnector::new("http2bin.org");
+    let mut client = SimpleClient::with_connector(connector).unwrap();
+
+    let response = client.get(b"/get", &[]).unwrap();
+
+    assert_eq!(response.stream_id, 1);
+    assert_eq!(response.status_code().unwrap(), 200);
+
+    // Dump the headers and the response body to stdout.
+    for header in response.headers.iter() {
+      println!("{}: {}",
+          str::from_utf8(&header.0).unwrap(),
+          str::from_utf8(&header.1).unwrap());
+    }
+
+    println!("{}", str::from_utf8(&response.body).unwrap());
+
+    // We can issue more requests after reading this one...
+    // These calls block until the request itself is sent, but do not wait
+    // for a response.
+    let req_id1 = client.request(b"GET", b"/get?hi=hello", &[], None).unwrap();
+    let req_id2 = client.request(b"GET", b"/asdf", &[], None).unwrap();
+
+    // Now we get a response for both requests... This does block.
+    let (resp1, resp2) = (
+        client.get_response(req_id1).unwrap(),
+        client.get_response(req_id2).unwrap(),
+    );
+
+    assert_eq!(resp1.status_code().unwrap(), 200);
+    assert_eq!(resp2.status_code().unwrap(), 404);
 }
